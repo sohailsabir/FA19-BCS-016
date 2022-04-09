79ea5b57bd664fc9fec6d1a258654ca62335c35b
Muhammad Sohail Sabir <sohailsabir6565@gmail.com>
2022-04-09 10:56:32 +0500

    change theme of app

--- bmicalculator/lib/main.dart
+++ bmicalculator/lib/main.dart
@@ -9,6 +9,10 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
