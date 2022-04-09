5fa95e629bd1a856496e1fe069d1d105e2f6c921
Muhammad Sohail Sabir <sohailsabir6565@gmail.com>
2022-04-09 10:12:37 +0500

    bmi calculator

---
+++ bmicalculator/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(BMICalculator());
+}
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text("Body"),
+      ),
+    );
+  }
+}
+
+
