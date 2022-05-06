e50fd0c6d11fc53c4e5adf6151be4146a30ec30b
Muhammad Sohail Sabir <sohailsabir6565@gmail.com>
2022-05-06 11:56:34 +0500

    Generate random Password

--- lab_mid_pocket_password_diary/lib/UI/simplePasswordScreen.dart
+++ lab_mid_pocket_password_diary/lib/UI/simplePasswordScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SimplePasswordScreen extends StatefulWidget {
@@ -8,7 +10,34 @@ class SimplePasswordScreen extends StatefulWidget {
 }
 
 class _SimplePasswordScreenState extends State<SimplePasswordScreen> {
-  TextEditingController password=new TextEditingController();
+  TextEditingController GeneratePassword=new TextEditingController();
+  String passworda()
+  {
+    const characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+    Random random = Random();
+    String a= String.fromCharCodes(Iterable.generate(4, (_) => characters.codeUnitAt(random.nextInt(characters.length))));
+    const number = '0123456789';
+    Random rand = Random();
+    String b= String.fromCharCodes(Iterable.generate(4, (_) => number.codeUnitAt(rand.nextInt(number.length))));
+    return a+b;
+
+
+  }
+  // String GeneratePassword(){
+  //   final Lenght=4;
+  //   final UpperCaseletter="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   final LowerCaseLetter="abcdefghijklmnopqrst";
+  //   final number="0123456789";
+  //   String char='';
+  //   char+="$UpperCaseletter$LowerCaseLetter";
+  //   return List.generate(Lenght, (index) {
+  //     final randomIndex=Random.secure().nextInt(char.length,);
+  //     return char[randomIndex];
+  //
+  //   }).join('');
+  //
+  //
+  // }
   bool check=false;
   @override
   Widget build(BuildContext context) {
@@ -22,8 +51,10 @@ class _SimplePasswordScreenState extends State<SimplePasswordScreen> {
         child: Column(
           children: [
             TextField(
-              controller: password,
+              obscureText: true,
+              controller: GeneratePassword,
               readOnly: true,
+              autofocus: true,
               decoration: InputDecoration(
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.pink,
@@ -47,6 +78,9 @@ class _SimplePasswordScreenState extends State<SimplePasswordScreen> {
                     setState(() {
                       check=true;
                     });
+                    final pass=passworda();
+                    GeneratePassword.text=pass;
+
                   },
                   child: Text("Create Password"),
                   style: ElevatedButton.styleFrom(
