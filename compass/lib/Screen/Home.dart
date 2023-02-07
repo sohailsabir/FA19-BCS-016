8923bcd96355a1f25810ef00baf360c9b6288711
Muhammad Sohail Sabir <sohailsabir6565@gmail.com>
2023-02-07 11:05:38 +0500

    update

--- compass/lib/Screen/Home.dart
+++ compass/lib/Screen/Home.dart
@@ -8,6 +8,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
         title: Text("Location Finder"),
         centerTitle: true,
